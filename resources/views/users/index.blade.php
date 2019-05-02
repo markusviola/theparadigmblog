a32c104dd71a93e8dda01679d2e8fd017c531d59
Markus Viola <markus.xamuel.viola@gmail.com>
2019-05-02 17:16:46 +0900

    admin can now enable/disable user

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -11,30 +11,32 @@
             <p>
             <table class="table">
                 <thead>
-                    <tr>
-                    <th scope="col">ID</th>
-                    <th scope="col">Username</th>
-                    <th scope="col">Email Address</th>
-                    <th scope="col">Date Registered</th>
-                    <th scope="col">Status</th>
+                    <tr class="d-flex">
+                    <th class="col-1" scope="col">ID</th>
+                    <th class="col-3" scope="col">Username</th>
+                    <th class="col-4" scope="col">Email Address</th>
+                    <th class="col-2" scope="col">Date Registered</th>
+                    <th class="col-2 text-center" scope="col">Status</th>
                     </tr>
                 </thead>
                 <tbody>
                     @foreach ($users as $user)
-                        <tr>
-                            <th scope="row">{{ $user->id }}</th>
-                            <td>{{ $user->username }}</td>
-                            <td>{{ $user->email }}</td>
-                            <td>{{ $user->created_at }}</td>
-                            <td> <button class="btn btn-primary">{{ $user->status }}</button> </td>
+                        <tr class="d-flex">
+                            <th class="col-1 align-middle" scope="row">{{ $user->id }}</th>
+                            <td class="col-3 align-middle">{{ $user->username }}</td>
+                            <td class="col-4 align-middle">{{ $user->email }}</td>
+                            <td class="col-2 align-middle">{{ $user->created_at }}</td>
+                            <td class="col-2 align-middle text-center"> 
+                                <form action="/users/{{ $user->id }}" method="POST">
+                                    @method('PATCH')
+                                    <button class="btn btn-{{ $user->status == "Active" ? "primary" : "danger" }}" type="submit" name="status" value="{{ $user->status }}">{{ $user->status }}</button> 
+                                    @csrf
+                                </form>    
+                            </td>
                         </tr>    
                     @endforeach
-                    
-                   
                 </tbody>
             </table>
         </div>
-
-        
     </div>
 @endsection
