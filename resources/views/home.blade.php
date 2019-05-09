46b8ac339df273cfc2dc5c0bcca27c4e9fe83a7d
markusviola <43871502+markusviola@users.noreply.github.com>
2019-05-09 14:12:30 +0900

    removed unused codes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,19 +18,3 @@
     </div>
 </div>
 @endsection
-
-{{-- For Reference:
-<div class="col-md-8">
-    <div class="card">
-        <div class="card-header"></div>
-
-        <div class="card-body">
-            @if (session('status'))
-                <div class="alert alert-success" role="alert">
-                    {{ session('status') }}
-                </div>
-            @endif
-            Everyone's blogs go here...
-        </div>
-    </div>
-</div> --}}
