783baec6077475769483ccb73b392d342bccf366
Markus Viola <markus.xamuel.viola@gmail.com>
2019-07-22 18:30:31 +0900

    added delete function for admin in actual posts

--- resources/views/comments/postIndex.blade.php
+++ resources/views/comments/postIndex.blade.php
@@ -1,15 +1,27 @@
 
 <hr class="mt-4">
+@include('comments.modals.delete')
 @foreach ($comments as $comment)
-    
         <div class="row">
             <div class="col-5">
                 <strong class="text-secondary">{{ $comment->user->username }}</strong>
             </div>
-            <div class="col-7 d-flex justify-content-end text-muted">
+            <div class="col d-flex justify-content-end text-muted">
                 Posted on {{ $comment->created_at }}
             </div>
         </div>
-        <div class="text-justify mt-2 long-text">{{ $comment->body }}</div>
+        {{-- need to adjust appropriately when switching account types --}}
+        <div class="row">
+            <div class="col-11 text-justify mt-2 long-text">{{ $comment->body }}</div>
+            @if(Auth::user() !== null && Auth::user()->isAdmin == 1)
+                <button class="col-1 trans-btn delete-modal" 
+                    data-toggle="modal" 
+                    data-target="#delete-confirm" 
+                    data-id="{{ $comment->id }}" 
+                    data-type="comment" 
+                ><i class="delete-post fas fa-minus-circle fa-lg"></i>
+                </button>
+            @endif
+        </div>
     <hr>
 @endforeach
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -5,7 +5,7 @@
         <div class="col-7">
             <br>
             <h2>{{ $post->title }}</h2>
-            <h5 class="text-muted">Posted by {{ $post->user->username }} on {{ $post->created_at }}</h5>
+            <h5 class="text-muted mt-2">Posted by {{ $post->user->username }} on {{ $post->created_at }}</h5>
             <hr>
             <br>
             <div class="text-justify long-text">{{ $post->body }}</div>
