832642a5c73c5a769839a7ad8511bb8b34db7471
Markus Viola <markus.xamuel.viola@gmail.com>
2019-08-13 17:44:11 +0900

    minor blade changes on register blade

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -32,7 +32,7 @@
                             <label for="email" class="col-md-4 col-form-label text-md-right alt-anti-neutral"><strong>{{ __('E-Mail Address') }}</strong></label>
 
                             <div class="col-md-6">
-                                <input id="email" type="text" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" autocomplete="email">
+                                <input id="email" type="text" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}">
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -46,7 +46,7 @@
                             <label for="password" class="col-md-4 col-form-label text-md-right alt-anti-neutral"><strong>{{ __('Password') }}</strong></label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required>
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -60,7 +60,7 @@
                             <label for="password-confirm" class="col-md-4 col-form-label text-md-right alt-anti-neutral"><strong>{{ __('Confirm Password') }}</strong></label>
 
                             <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
                             </div>
                         </div>
 
