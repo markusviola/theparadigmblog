5d99e0448246df5609a1827f29646d17aa31cc11
Markus Viola <markus.xamuel.viola@gmail.com>
2019-08-13 17:53:59 +0900

    implemented proper input error handling on post forms

--- resources/views/posts/form.blade.php
+++ resources/views/posts/form.blade.php
@@ -1,25 +1,34 @@
-<div>{{ $errors->first('title') }}</div>
+
 <div class="form-group">
     <input
-        class="form-control my-4"
+        class="form-control mt-4 @error('title') is-invalid @enderror"
         type="text"
         id="title"
         placeholder="Make a good title..."
         name="title" value="{{ old('title') ?? $post->title  }}"
         onkeyup="mirrorTitle(this)"
     >
+    @error('title')
+        <span class="invalid-feedback mb-4" role="alert">
+            <strong>{{ $message }}</strong>
+        </span>
+    @enderror
 </div>
 
-<div>{{ $errors->first('body') }}</div>
 <div class="form-group">
     <textarea
         onkeyup="mirrorMarkDown(this)"
-        class="preserve-breaks form-control"
+        class="preserve-breaks form-control @error('body') is-invalid @enderror"
         placeholder="Write your thoughts here..."
         name="body"
         id="body"
         cols="30"
         rows="29"
     >{{ old('body') ?? $post->body }}</textarea>
+    @error('body')
+        <span class="invalid-feedback" role="alert">
+            <strong>{{ $message }}</strong>
+        </span>
+    @enderror
 </div>
 @csrf
